01d1488901f349e6f42993414284ffe3ec4c1cb7
hp <hp@LAPTOP-EHROU7C4>
2022-08-23 20:19:20 +0530

    first commit

---
+++ SDP PROJECT/src/PREETHI.java
@@ -0,0 +1,20 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>dynamic font</title>
+	<style type="text/css">
+	.rt{font=times new roman}
+	.wt{color='blue'}
+	</style>
+	</head>
+	<body>
+	<p align="center" class="rt">
+	I am from <font class="wt";
+	onmouseover="this.style.color='blue';
+	this.style.font='italic 30pt monotype corsiva';"
+	onmouseout="this.style.color='red';
+	this.style.font='normal 16pt times new roman';">REVA UNIVERSITY BANGLORE</font>
+	</p>
+	</body>
+	</html>
+
